8c4c3673f526ea866a416efd1b3a3ae00efefae2
ckp <ckp4392@gmail.com>
2023-03-11 12:25:08 +0530

    classes and constructors

---
+++ Python_Oops/classes.py
@@ -0,0 +1,51 @@
+"""
+classes = real world entity or object
+attributes : - properties of the class , in Car class -windows,doors,enginetype etc
+Methods :->> actions of the class.
+"""
+"""
+# This is the better way of writing class
+'__init__' is a constructor 
+'window' will be passed at run time but in 'self.windows' `windows` is an attribute of class.
+both can be of different name.
+why 'self' is used ?? 
+ANS:->> using self we can call any attributes of Car class.
+Works of constructor : ---> As soon as we create an object of Car class , 
+all the default attributes will be assigned accordingly 
+for example 
+car1 = Car(4,4,'petrol') in this case , constructor will assign 
+windows=4
+doors=4
+enginetype=petrol
+"""
+
+class Car:
+    #Constructor
+    def __init__(self,window,door,enginetype):
+        self.darwaja = window
+        self.doors = door
+        self.enginetype = 'petrol'
+
+    def self_driving1(self):
+        # why we are writing 'self' here bcz we can use all those attributes 
+        # of Car class in this method also
+        print("The car type is {} engine ".format(self.enginetype))
+        # in the above print statement we are using attribute 'enginetype' which is assigned earlier
+
+        """
+           we can give another parameter also in method 'self_driving'
+           let's say we want it to be 'electric' car in 'self_driving' case 
+           we can use it like below
+        """     
+    def self_driving2(self,engine):
+        print("The car type is {} engine ".format(engine))        
+
+car1 = Car(4,4,'petrol')      
+print("The number of windows of the Car is : ", car1.darwaja)  
+car1.self_driving1() # here no need to pass anything while calling the car1 object it will take enginetype 
+                    #from earlier assigned value due to 'self'
+
+car1.self_driving2('electric') # here we are passing one more parameter cause we have used 
+                                # extra parameter in  method 'self_driving2'
+
+        
---
+++ Python_Oops/constructors.py
@@ -0,0 +1,48 @@
+""" 
+************************************************************************************************
+Ques: Can we write multiple constructors in python ?? 
+ANs : We can not create multiple constructors(__init__ function) in python 
+      because if we make multiple constructors then python will consider the 
+      last constructor as final bcz last constructor will override all the previous 
+      constructors.
+      In other programming language like Java and Dot net we can create multiple constructors but, 
+      not in python.
+      we will see it with one example :-->>>> Check below
+**************************************************************************************************      
+"""
+class Animal:
+
+    #creating first constructor
+    def __init__(self,name,species):
+        self.name = name
+        self.species = species
+
+    #creating second constructor
+    def __init__(self,name,species,age):
+        self.name = name
+        self.species = species
+        self.age = age
+
+    #creating a method just like that,its not important to create it to understand above question    
+    def make_sound(self,sound):
+        return "The animal is {} and it makes sound {}".format(self.name,sound)       
+
+# creating an object of Animal class:
+#this object is created keeping first constructor in mind
+# that's why only two attributes have been passed while making object
+
+dog = Animal("dog","mammalas") 
+print(dog) 
+
+"""
+Traceback (most recent call last):
+  File "c:/Users/ckp43_000/python_programming/python_programming/Python_Oops/constructors.py", line 33, in <module>
+    dog = Animal("dog","mammalas")
+TypeError: __init__() missing 1 required positional argument: 'age'
+"""
+# when we execute line number 35 , we get above error message 
+# which indicates that class 'Animal' is considering the second constructor and thats 
+# why it's asking for third argument/attribute which is 'age' which is defined in second constructor.
+
+
+
