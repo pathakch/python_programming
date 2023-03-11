b52e037e9e97acb124311152e932363d565556f3
ckp <ckp4392@gmail.com>
2023-03-11 13:45:16 +0530

    creating multiple constructors-method explained

--- Python_Oops/constructors.py
+++ Python_Oops/constructors.py
@@ -31,8 +31,8 @@ class Animal:
 # this object is created keeping first constructor in mind
 # that's why only two attributes(name='dog' and species='mammalas') have been passed while making object
 
-#dog = Animal("dog","mammalas") 
-#print(dog) 
+dog = Animal("dog","mammalas") 
+print(dog) 
 
 """
 Traceback (most recent call last):
@@ -53,17 +53,59 @@ dog2 = Animal("dog","mammalas",20)
 print(dog2)
 sound = dog2.make_sound("woof woof")
 print(sound) 
+
 """
 Output :-->>
 PS C:\Users\ckp43_000\python_programming\python_programming\Python_Oops> & C:/Anaconda3/envs/new_pychrm/python.exe c:/Users/ckp43_000/python_programming/python_programming/Python_Oops/constructors.py
 <__main__.Animal object at 0x0000006122C312C8>
 The animal is dog and it makes sound woof woof
-"""
-"""
+
 Explanation :--->>>
 creating object of Animal class keeping second constructor in mind 
 and that's why passing total three attributes(name,species,age) while making object
 and we got the output as expecteed, it means class Animal is considering 
-the second constructor as final constructor"""
+the second constructor as final constructor
 
+*******************************************************************************************
+********************************************************************************************
+Regarding above question : if we really want to create multiple constructors in python class
+then there is a method for that , we can use '*args' with 'if/elif/else' condition and pass
+as many attributes as we want while creating object for that class
+Check below example :-->>>
+********************************************************************************************
+"""
+#creating a new class just to show how to implement multiple constructors in python class
+#Though its not a good practice to create multiple constructors in python class.
+class New_Animal:
+
+    #creating constructor
+    def __init__(self,*args):
+        if len(args) == 1:
+            self.name = args[0]
+        elif len(args) == 2:
+            self.name = args[0]
+            self.species = args[1]
+        elif len(args) == 3:
+            self.name = args[0]
+            self.species = args[1]
+            self.age = args[2]    
+
+# creating object of 'New_Animal' class while passing one attribute
+dog3 = New_Animal('dog')
+print(dog3.name)
+
+# Creating an object of 'New_Animal' class while passing two attributes
+dog4 = New_Animal('dog','mammalas')
+print("name of animal is :",dog4.name,"\nAnd species of animal is :",dog4.species)
+
+"""
+Output : -->>
+PS C:\Users\ckp43_000\python_programming\python_programming\Python_Oops> & C:/Anaconda3/envs/new_pychrm/python.exe c:/Users/ckp43_000/python_programming/python_programming/Python_Oops/t.py
+dog
+name of animal is : dog
+And species of animal is : mammalas
+
+Explanation :--->>
+In above output we can see that constructor is working as expected with multiple attributes as well.
+"""
 
