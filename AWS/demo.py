79d232d88cc4c20ee32ad69b4322586472d31064
pathakch <ckp4392@gmail.com>
2024-02-24 20:20:43 +0530

    s3 file reading from local python program

---
+++ AWS/demo.py
@@ -0,0 +1,8 @@
+import boto3
+import json
+s3 = boto3.resource('s3')
+bucket_name = 'myfirst-demo-s3bucket'
+file_object = s3.Object(bucket_name,'test_json_file.json')
+contents = file_object.get()['Body'].read().decode('utf-8')
+json_data = json.loads(contents)
+print(json_data['info'])
