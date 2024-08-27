2fc907c235484183eece98a5085696894a639d75
pathakch <ckp4392@gmail.com>
2024-08-27 08:32:10 +0530

    modified

--- AWS/boto3/read_s3.py
+++ AWS/boto3/read_s3.py
@@ -2,8 +2,13 @@ import boto3
 import json
 
 s3 = boto3.resource('s3')
-bucket_name = 'myfirst-demo-s3bucket'
-file_object = s3.Object(bucket_name,'test_json_file.json')
+
+bucket_name = 'my-demo-bucket123321'
+
+file_object = s3.Object(bucket_name,'information.txt')
+
 contents = file_object.get()['Body'].read().decode('utf-8')
+
 json_data = json.loads(contents)
+
 print(json_data['info'])
